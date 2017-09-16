506c9fc3ca1ed2d76dbb3bc60dd07407931ad52c
Ardjlon <carlosardany@gmail.om>
2017-09-16 12:52:32 -0600

    Initial commit

---
+++ substratum.ardjlon.darkelegance/build/generated/source/buildConfig/debug/substratum/ardjlon/darkelegance/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package substratum.ardjlon.darkelegance;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "substratum.ardjlon.darkelegance";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final byte[] DECRYPTION_KEY = {62, 90, -120, 78, 114, -96, -71, -117, -51, -99, -63, -62, -74, 72, 41, -21};
+  public static final byte[] IV_KEY = {70, -24, -14, -121, -59, -78, -78, -52, -86, -33, 122, -7, 126, -51, 88, -121};
+  public static final boolean SUPPORTS_THIRD_PARTY_THEME_SYSTEMS = false;
+}
